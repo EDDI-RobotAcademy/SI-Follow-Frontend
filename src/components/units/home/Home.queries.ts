c4a125b2b8ab29b3df0a2d59b312f781f3a64a08
0H00NI <yhoon3002@gmail.com>
2024-10-12 18:43:07 +0900

    feat: [임영훈] 사용자가 로그인 화면에서 Git 아이콘 클릭시 Git 로그인이 정상적으로 됩니다. [SIFFR-11]

--- src/components/units/home/Home.queries.ts
+++ src/components/units/home/Home.queries.ts
@@ -7,3 +7,11 @@ export const GET_KAKAO_OAUTH_URI = async () => {
             window.location.href = res.data.url;
         });
 };
+
+export const GET_GITHUB_OAUTH_URI = async () => {
+    return await axiosInstance.djangoAxiosInst
+        .get("/github_oauth/github")
+        .then((res) => {
+            window.location.href = res.data.url;
+        });
+};
