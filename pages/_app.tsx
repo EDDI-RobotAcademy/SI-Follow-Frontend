d878371a216e2069f1aee5f7b50675fdaca4e295
0H00NI <yhoon3002@gmail.com>
2024-10-14 15:07:09 +0900

    feat: [임영훈] 전역 변수 설정을 위한 RecoilRoot 설정 [SIFFR-X]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import { Global } from "@emotion/react";
 import { globalStyles } from "@/commons/styles/globalStyles/globalStyles";
 
 function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Global styles={globalStyles} />
-            <Component {...pageProps} />
+            <RecoilRoot>
+                <Global styles={globalStyles} />
+                <Component {...pageProps} />
+            </RecoilRoot>
         </>
     );
 }
