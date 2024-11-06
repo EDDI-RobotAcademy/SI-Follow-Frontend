1ceab5115ca235894de08eb9ffc06e25b496c663
0H00NI <yhoon3002@gmail.com>
2024-11-06 10:51:10 +0900

    feat: [임영훈] MainPage 리셋 후 커밋 [SIFFR-X]

---
+++ src/components/units/home/introduce/Introduce.container.tsx
@@ -0,0 +1,9 @@
+import IntroducePresenter from "./Introduce.presenter";
+
+export default function IntroduceContainer() {
+    return (
+        <>
+            <IntroducePresenter />
+        </>
+    );
+}
--- src/components/units/home/model/Model.styles.ts
+++ src/components/units/home/model/Model.styles.ts
@@ -1,7 +1,25 @@
 import styled from "@emotion/styled";
+import { Box } from "@mui/material";
 
-export const HeroContainerContainer = styled.div`
-    width: 33.4rem;
-    height: 21.6rem;
-    overflow-y: hidden;
+export const IntroudceWrapper = styled(Box)`
+    height: auto;
+`;
+
+export const Div123 = styled(Box)`
+    width: 24.5px;
+    max-width: 24.5px;
+    height: 14px;
+    max-height: 14px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    font-size: 0.1rem;
+    padding-bottom: 0.5px;
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
 `;
